c00cc5fef366911a6cfb2aa3bbace39e97f1db71
Umba joseph <Josephdjamba04@gmail.com>
2024-08-05 14:24:00 +0200

    Add/Front-end part one and functionaliy

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import './App.css';
+import InputFeild from './components/InputFeild';
+
 
 
 const App: React.FC = () => {
+
+  const [todo, setTodo] = useState<string>("");
+  const [todos,setTodos] = useState<Todo[]>([]);
+
+ const handleAdd = () => {}
+
   return (
     <div className="App">
      <span className='heading'>Taskify</span>
+     <InputFeild todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
     </div>
   );
 }
