d9b9602e13b9b0c0c56e988a840fad2003a708f4
Umba joseph <Josephdjamba04@gmail.com>
2024-08-07 13:10:02 +0200

    Edit function added

--- src/components/InputFeild.tsx
+++ src/components/InputFeild.tsx
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import "./style.css"
 
 interface Props {
     todo:string,
     setTodo:React.Dispatch<React.SetStateAction<string>> ,
-    handleAdd : () => void,
+    handleAdd : (e:React.FormEvent) => void,
 
 }
 
 const InputFeild: React.FC<Props> = ({todo,setTodo, handleAdd}) => {
 
-  return <form className='input' onSubmit={handleAdd} > 
+const inputRef = useRef<HTMLInputElement>(null);
 
-  <input type="text" value={todo} onChange={(e)=> setTodo(e.target.value)} placeholder="Enter a task" className='input__box' />
+  return <form className='input' onSubmit={ (e) => {handleAdd(e); inputRef.current?.blur();}} > 
 
+  <input 
+  ref={inputRef}
+  type="text"
+   value={todo}
+   onChange={(e)=> setTodo(e.target.value)} 
+   placeholder="Enter a task" 
+   className='input__box' />
   <button title="Submit form" className='input__submit' type='submit'> Go</button>
 
    </form>
